87f6cde498964cc0d4bfb34f047ee2831065b221
gggfffeeeppp <549819224@qq.com>
2020-08-12 17:44:31 +0800

    testDemo

---
+++ src/HiWorld.java
@@ -0,0 +1,20 @@
+/**
+ * FileName: HiWorld
+ * Author:   蔡志勇
+ * Date:     2020/8/12 17:41
+ * Description:
+ * History:
+ */
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ * @author 蔡志勇
+ * @date 2020/8/12
+ * @version 1.0.0
+ */
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi World");
+    }
+}
